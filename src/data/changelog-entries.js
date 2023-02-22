650696e22ad1aa7f84ab666a31958f98a53cb782
Peppe L-G <peter.larsson.green@gmail.com>
2023-02-22 09:05:03 +0100

    Extracted changelog entries to it's own data JS file, so the changelog page is generated dynamically.

---
+++ src/data/changelog-entries.js
@@ -0,0 +1,23 @@
+export const changelogEntries = [{
+	version: "1.1.1",
+	date: "2023-02-21",
+	changeDescriptions: [
+		`Changed it so it runs on <a href="https://peterlg.se/" target="_blank">peterlg.se</a> instead of <a href="https://peppel-g.github.io/peterlg" target="_blank">peppel-g.github.io/peterlg</a>`,
+	],
+}, {
+	version: "1.1.0",
+	date: "2023-02-15",
+	changeDescriptions: [
+		`Made it deployable to <a href="https://pages.github.com/" target="_blank">GitHub Pages</a>`,
+	],
+}, {
+	version: "1.0.0",
+	date: "2023-02-12",
+	changeDescriptions: [
+		`Added start page with latest project`,
+		`Added projects page (for now only one project; the project for this website)`,
+		`Added project page (view project with specific id)`,
+		`Added about page`,
+		`Added changelog page`,
+	],
+}]
--- src/routes/changelog/+page.svelte
+++ src/routes/changelog/+page.svelte
@@ -1,24 +1,23 @@
+<script>
+	import { changelogEntries } from "data/changelog-entries.js"
+</script>
+
 <h1>Changelog</h1>
 <p>This changelog follow the <a href="https://semver.org/" target="_blank">SemVer</a> convention.</p>
 
-<h2><span class="version">1.1.1</span> <span class="date">2023-02-21</span></h2>
-<ul>
-	<li>Changed it so it runs on <a href="https://peterlg.se/" target="_blank">peterlg.se</a> instead of <a href="https://peppel-g.github.io/peterlg" target="_blank">peppel-g.github.io/peterlg</a></li>
-</ul>
-
-<h2><span class="version">1.1.0</span> <span class="date">2023-02-15</span></h2>
-<ul>
-	<li>Made it deployable to <a href="https://pages.github.com/" target="_blank">GitHub Pages</a></li>
-</ul>
-
-<h2><span class="version">1.0.0</span> <span class="date">2023-02-12</span></h2>
-<ul>
-	<li>Added start page with latest project</li>
-	<li>Added projects page (for now only one project; the project for this website)</li>
-	<li>Added project page (view project with specific id)</li>
-	<li>Added about page</li>
-	<li>Added changelog page</li>
-</ul>
+{#each changelogEntries as entry}
+	
+	<h2>
+		<span class="version">{entry.version}</span>
+		<span class="date">{entry.date}</span>
+	</h2>
+	<ul>
+		{#each entry.changeDescriptions as changeDescription}
+			<li>{@html changeDescription}</li>
+		{/each}
+	</ul>
+	
+{/each}
 
 <style>
 	
